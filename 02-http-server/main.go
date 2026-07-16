d15ba47601ed32c5a71df58b4390a16991c2453f
Saikiran Patil <84263946+saikiranpatil@users.noreply.github.com>
2026-07-16 23:27:27 +0530

    chapter 1: HTTP streams

--- 02-http-server/main.go
+++ 02-http-server/main.go
@@ -1,7 +1,57 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	ch := make(chan string)
+
+	go func() {
+		defer close(ch)
+		defer f.Close()
+
+		str := ""
+		for {
+			data := make([]byte, 8)
+			n, err := f.Read(data)
+			if err != nil {
+				break
+			}
+
+			message := string(data[:n])
+
+			if idx := strings.Index(message, "\n"); idx != -1 {
+				str += message[:idx]
+				message = message[idx+1 : n]
+
+				ch <- str
+				str = ""
+			}
+
+			str += message
+		}
+
+		if len(str) != 0 {
+			ch <- str
+		}
+	}()
+
+	return ch
+}
 
 func main() {
-	fmt.Println("I hope I get the job!")
+	file, err := os.Open("messages.txt")
+	if err != nil {
+		fmt.Println("error while reading file")
+	}
+
+	ch := getLinesChannel(file)
+
+	for messages:= range ch {
+		fmt.Printf("read: %s\n", messages)
+	}
 }
